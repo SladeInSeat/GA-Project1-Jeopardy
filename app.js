595a65ab22a022c89ccd08c610f6666686aaa996
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-11 13:06:46 -0400

    loops now give category columns and question boxes unique ids based on 4 digits; 0000; 1st is column # next 3 are point values

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+
+
+//  Build Columns and QA Boxes that fill out game container. Hardcoded Categories
+//  and point values.
+
 let gameContainer = document.getElementsByClassName('gameContainer')[0];
 
 for(let i = 0;i < 5;i++){
     let newColumn = document.createElement('div');
-    newColumn.className = 'categoryColumn';
+    newColumn.setAttribute('class','categoryColumn');
+    newColumn.setAttribute('id','category' + i);
     gameContainer.appendChild(newColumn);
-    for(let i = 0; i < 6; i++){
+    for(let y = 0; y < 6; y++){
         let newBox = document.createElement('div');
-        newBox.className = 'questionBox';
-        if(i == 0){
+        newBox.setAttribute('class','questionBox');
+        if(y == 0){
             newBox.textContent = 'Category'
-        } else{newBox.textContent = i*100;}
+        } else{newBox.textContent = y*100;};
+        iString = i.toString()
+        newBox.setAttribute('id',iString.concat(y*100));
         newColumn.appendChild(newBox);
     }
 
