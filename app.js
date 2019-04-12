9c30a33d020aa87786ea89e0b7aa57858e3f81c7
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-04-11 20:32:20 -0400

    removed button html, event listener on answer divs consolelogs textContent

--- app.js
+++ app.js
@@ -19,42 +19,50 @@ for(let i = 0;i < 5;i++){
         newBox.setAttribute('id',iString.concat(y*100));
         newColumn.appendChild(newBox);
         newBox.addEventListener('click',populateModal);
-
     }
 
 };
 
-//  modal creation
+//  modal object, can it stay out here? i want to put it in the functions that use it later
 let modalObj = document.getElementsByClassName('modalContainer')[0];
-let buttonObj = document.getElementById('modalBtn');
 
-buttonObj.addEventListener('click', populateModal);
+//  Event listeners
+    //  Event listener for quesiton Div
+// buttonObj.addEventListener('click', populateModal);
 
+    //  close modal window by clicking it
+window.addEventListener('click',function(event){
+    if(event.target == modalObj){
+        modalObj.style.display = 'none';
+    }
+});    // window. and event.target credited to https://www.w3schools.com/howto/howto_css_modals.asp
+
+//  Populates modal div by geting id from this. div, using id to look up data from
+//  questionObjects array, populates questions and answers with data, creates 
+//  event listeners on answer divs.
 function populateModal (){
     modalObj.style.display = 'flex';
     idString = this.getAttribute('id');
+    console.log(idString)
     let questionContainer = document.getElementsByClassName('question')[0];
     let quesitonText = questionObjects[idString].question;
+    console.log(quesitonText)
     questionContainer.textContent = quesitonText;
     for(let i = 0; i < 4; i++){
         let answerContainer = document.getElementsByClassName('answer')[i];
         let answerText = questionObjects[idString].answers[i];
         answerContainer.textContent = answerText;
+        answerContainer.addEventListener('click',answerTruthPointsClear);
+        //  add eventlistener here? it will check if answer is correct,
+        //  add points or subtract points, clear modal of data, close modal
     }
 }
 
+function answerTruthPointsClear(){
+    selectedAnswer = this.textContent;
+    console.log(selectedAnswer);
+};
 
-function getSelfID (){
-    idString = this.getAttribute('id');
-    console.log(idString);
-    console.log(typeof(idString));
-}
-//  close modal window by clicking it
-window.addEventListener('click',function(event){
-    if(event.target == modalObj){
-        modalObj.style.display = 'none';
-    }
-}); // window. and event.target credited to https://www.w3schools.com/howto/howto_css_modals.asp
 
 
 questionObjects = {
@@ -67,5 +75,10 @@ questionObjects = {
                     question:   "Who won 2019 NCAA MBB Tourney?",
                     answers:    ["UNC","UVA","TT","Pitt"],
                     correntAnswer:  "UVA"
+                },
+    '0300'  :   {   pointValue: 300,
+                    question:   "Are you serious?",
+                    answers:    ['Maybe','You betcha','are you???','Yes'],
+                    correctAnswer:  'You betcha'
                 }    
 }
